e3f1f750a3e795785a51d28942ea7beab6019823
clauk <clauk.c@gmail.com>
2013-06-03 09:20:49 -0700

    Auto Generate Folder for benchmark result

--- dist-run-all.sh
+++ dist-run-all.sh
@@ -4,21 +4,29 @@ ps aux |grep java | grep barrier|awk '{print $2}'|xargs kill -9
 rm results
 rm log*
 
-java -jar barrier.jar 4 & # 3 means number of servers in the system
+java -Xms512M -Xmx2G  -jar barrier.jar 4 > log_barrier 2>&1 & # 3 means number of servers in the system
 sleep 2
 echo "barrier started"
 
-#start=$(date +%s)
+declare -i SERVER_NUM
+declare -i NODE_NUM_PER_SERVER
+declare -i START_SERVER_IP
 
-JAR_PATH=/home/chen/XLW/experiment/trick/SyntheticNetworkGen # change accordingly
+SERVER_NUM=4
+NODE_NUM_PER_SERVER=16384
+START_SERVER_IP=11
+
+JAR_PATH=/home/chen/XLW_SyntheticNetwork # change accordingly
 # don't need scp, cause home dir is sync in all sysnet servers
-ssh chen@compute-0-11.local "cd $JAR_PATH; java -Xms512M -Xmx2G -jar $JAR_PATH/server.jar 0 0 > log0 2>&1" &
-ssh chen@compute-0-12.local "cd $JAR_PATH; java -Xms512M -Xmx2G -jar $JAR_PATH/server.jar 1 16384 > log1 2>&1" &
-ssh chen@compute-0-13.local "cd $JAR_PATH; java -Xms512M -Xmx2G -jar $JAR_PATH/server.jar 2 32768 > log2 2>&1" &
-ssh chen@compute-0-14.local "cd $JAR_PATH; java -Xms512M -Xmx2G -jar $JAR_PATH/server.jar 3 49152 > log3 2>&1" &
+for ((SERVER_ID=0;SERVER_ID<$SERVER_NUM; SERVER_ID++))
+  do
+    declare -i NODE_START_ID
+    NODE_START_ID=NODE_NUM_PER_SERVER\*SERVER_ID
+    declare -i SERVER_IP
+    SERVER_IP=START_SERVER_IP+SERVER_ID
+    LOG=log$SERVER_ID
+    echo "server ip: $SERVER_IP  start node id: $NODE_START_ID  log: $LOG"
+    ssh chen@compute-0-$SERVER_IP.local "ps aux|grep java |grep server.jar | awk '{print $2}'|xargs kill -9;"
+    ssh chen@compute-0-$SERVER_IP.local " cd $JAR_PATH; echo `hostname -a`; java -Xms512M -Xmx2G -jar $JAR_PATH/server.jar $SERVER_ID $NODE_START_ID > $LOG 2>&1 &" &
+  done
 echo "finish invoking servers"
-
-#end=$(date +%s)  
-#time=$(( $end - $start ))  
-#echo "Run time: " $time  
-
